c436c7ab523a1ed79961dd3ef3e6a38666256589
scarlettjohanssons <wolverinelop@gmail.com>
2021-06-03 14:39:47 +0300

    Updates

---
+++ precache-manifest.ae95ba87329981d5c258b78443402efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25923207611ee2d492e466fdd29b654",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41e063cfda28734ebc00",
+    "url": "./static/css/main.36ffb99c.chunk.css"
+  },
+  {
+    "revision": "2be444cabb932727fd12",
+    "url": "./static/js/2.52b26eb6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.52b26eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e063cfda28734ebc00",
+    "url": "./static/js/main.67bd7a64.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
